7edaa460aa931d89b1b06d88510f3c6362a6df4e
Bastien-DA <deamezagabastien@outlook.fr>
2025-03-31 14:19:36 +0200

    fix: eslint

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,30 +1,26 @@
-import { useEffect } from "react";
 import { useCurrentTrack } from "@api/getCurrentTrack";
 import { useRecentlyPlayedTracks } from "@api/getRecentlyPlayedTrack";
-import { useStore } from "~/store/zustand";
 import HomeScreen from "@screens/HomeScreen";
+import { useEffect } from "react";
+
+import { useStore } from "~/store/zustand";
 
 export default function Home() {
-  const setCurrentTrack = useStore((state) => state.setCurrentTrack)
-  const setRecentlyPlayedTracks = useStore((state) => state.setRecentlyPlayedTracks)
-  const currentTrack = useCurrentTrack(1000)
-  const recentlyPlayedTracks = useRecentlyPlayedTracks({ limit: 10 })
-  console.log(recentlyPlayedTracks)
+  const setCurrentTrack = useStore((state) => state.setCurrentTrack);
+  const setRecentlyPlayedTracks = useStore((state) => state.setRecentlyPlayedTracks);
+  const currentTrack = useCurrentTrack(1000);
+  const recentlyPlayedTracks = useRecentlyPlayedTracks({ limit: 10 });
+  console.log(recentlyPlayedTracks);
 
   useEffect(() => {
-
     // Mettre à jour le morceau en cours
     if (currentTrack?.data) {
-      setCurrentTrack(currentTrack.data)
+      setCurrentTrack(currentTrack.data);
     }
     if (recentlyPlayedTracks?.data) {
-      setRecentlyPlayedTracks(recentlyPlayedTracks.data.items)
+      setRecentlyPlayedTracks(recentlyPlayedTracks.data.items);
     }
-  }, [
-    currentTrack,
-    recentlyPlayedTracks,
-    setRecentlyPlayedTracks
-  ]);
+  }, [currentTrack, recentlyPlayedTracks, setRecentlyPlayedTracks]);
 
   return <HomeScreen />;
 }
